1ce30eb5b0fb324729553df760c730f905632a9c
uchansansan <uchansansan@gmail.com>
2023-09-19 10:37:49 +0700

    task 3 done

--- task03.py
+++ task03.py
@@ -2,3 +2,12 @@
 Задание 3.
 Дан текст. Определить, сколько различных букв в нем.
 '''
+text = input()
+letters = {}
+
+for i in text:
+    if i not in letters.keys() and i.isalpha():
+        letters[i] = 1
+    elif i.isalpha():
+        letters[i] += 1
+print(letters[max(letters)])
