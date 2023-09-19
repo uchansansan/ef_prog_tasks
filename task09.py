b1acedfe788b4978f05643d4c140bb5bc11bd452
uchansansan <uchansansan@gmail.com>
2023-09-20 00:50:27 +0700

    task 9 done

--- task09.py
+++ task09.py
@@ -2,3 +2,8 @@
 Задание 9.
 Дано предложение. В нем только два слова одинаковые. Найти эти слова.
 '''
+# sentence = 'мама ghbdtn мама привет'
+sentence = input()
+for word in sentence.split():
+    if word in sentence.replace(word, '', 1):
+        print(word)
