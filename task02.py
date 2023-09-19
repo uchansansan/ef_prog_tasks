ab113dfd0efc2c0f502b0e97fd35e688283d88e9
uchansansan <uchansansan@gmail.com>
2023-09-19 10:32:18 +0700

    task 2 done

--- task02.py
+++ task02.py
@@ -2,3 +2,14 @@
 Задание 2.
 Дан текст. Необходимо определить максимальное количество последовательных одинаковых символов в нём.
 '''
+# text = 'мама ммммыла рамууууу'
+text = input()
+num = 1
+max_global = 0
+for i in range(0, len(text) - 1):
+    if text[i] == text[i + 1]:
+        num += 1
+        max_global = max(max_global, num)
+    else:
+        num = 1
+print(max_global)
