f749b2269aedc59cdcee81f1ca5aabddd99eeaf7
uchansansan <uchansansan@gmail.com>
2023-09-20 00:43:02 +0700

    task 8 done

--- task08.py
+++ task08.py
@@ -2,3 +2,7 @@
 Задание 8.
 Дано предложение. Напечатать все его слова в порядке неубывания их длин.
 '''
+# sentence = 'мама ыл раму'
+sentence = input()
+sorted_word_list = sorted(sentence.split(), key=len)
+print(' '.join(sorted_word_list))
