16916473ad553450bafdf23440f86f1a1a94d81d
uchansansan <uchansansan@gmail.com>
2023-09-20 01:03:03 +0700

    task 10 done

--- task10.py
+++ task10.py
@@ -1,5 +1,18 @@
 '''
 Задание 10.
 Дано предложение. Выведите на экран те слова, которые отличны от первого слова,
-в слове нет повторяющихся букв и это слово симметрично.
+в слове нет повторяющихся букв.
 '''
+# sentence = 'марш привет марш*'
+sentence = input()
+
+new_sentence = ''
+for char in sentence:
+    if not char.isalpha():
+        new_sentence = sentence.replace(char, '')
+
+words = new_sentence.split()
+
+for word in words:
+    if word != words[0] and len(word) == len(set(word)):
+        print(word)
