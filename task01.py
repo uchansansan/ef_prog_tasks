8fcc80b7e257ffc005d8bd5ae30b0eddd3a2cb62
uchansansan <uchansansan@gmail.com>
2023-09-19 10:33:13 +0700

    task 1 done

--- task01.py
+++ task01.py
@@ -2,3 +2,13 @@
 Задание 1.
 Дан текст. Необходимо определить максимальное количество последовательных пробельных символов  в нём.
 '''
+text = input()
+num = 1
+max_global = 0
+for i in range(0, len(text) - 1):
+    if text[i] == text[i + 1] and text[i] == ' ':
+        num += 1
+        max_global = max(max_global, num)
+    else:
+        num = 1
+print(max_global)
