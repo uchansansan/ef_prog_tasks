b07030cf6f647dfdf2fe332c93f8936beea884c0
uchansansan <uchansansan@gmail.com>
2023-09-20 00:32:39 +0700

    task 4 done

--- task04.py
+++ task04.py
@@ -3,3 +3,14 @@
 Определите какой символ встречается ровно три раза в тексте. Гарантируется, что символ
 с такой частотой встречаемости только один.
 '''
+# text = 'uvvvu'
+text = input()
+chars = {}
+for char in text:
+    if char in chars:
+        chars[char] += 1
+    else:
+        chars[char] = 1
+    if chars[char] == 3:
+        print(char)
+        break
