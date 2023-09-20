422e687589e8c79c923a55251f434addb1279ca4
uchansansan <uchansansan@gmail.com>
2023-09-20 10:23:11 +0700

    task 2 done

--- task02.py
+++ task02.py
@@ -9,7 +9,7 @@ max_global = 0
 for i in range(0, len(text) - 1):
     if text[i] == text[i + 1]:
         num += 1
-        max_global = max(max_global, num)
     else:
+        max_global = max(max_global, num)
         num = 1
 print(max_global)
