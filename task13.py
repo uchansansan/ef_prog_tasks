4ac6c8448e0efd3d1cec394e7035e378e948e7a8
uchansansan <uchansansan@gmail.com>
2023-09-24 23:50:12 +0700

    task 13 done

--- task13.py
+++ task13.py
@@ -7,3 +7,28 @@
 Ввод номеров должен завершить тогда, когда будет введен "счастливый" билет.
 Программа должна вывести его порядковый номер. Счет начинается с 1.
 '''
+cnt = 0
+lucky = False
+
+def lucky_check(ticket):
+    lenth = len(ticket)
+    if lenth % 2 == 0:
+        part1 = ticket[:lenth // 2]
+        part2 = ticket[lenth // 2:]
+        part1_sum, part2_sum = 0, 0
+
+        for i in range(lenth // 2):
+            part1_sum += int(part1[i])
+            part2_sum += int(part2[i])
+
+        return part1_sum == part2_sum
+    else:
+        return False
+
+
+while not lucky:
+    ticket = input()
+    lucky = lucky_check(ticket)
+    cnt += 1
+else:
+    print(cnt)
